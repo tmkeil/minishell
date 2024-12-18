714444d1f50c79ad9dddb481d836fe8c1cd82a1d
frocha <frocha@student.42heilbronn.de>
2024-12-18 19:34:48 +0100

    export now working with parameters as well as multiple parameters, edge cases have not been tested/handled yet

--- srcs/execution.c
+++ srcs/execution.c
@@ -47,8 +47,35 @@ void	ft_execute(t_lexems *lexems, char *cmd, char **envp)
 	clean_args(args);
 }
 
+int is_valid_env_name(char *name) {
+    int i;
+
+	i = 0;
+    if (!name || !(ft_isalpha(name[0]) || name[0] == '_'))
+        return 0;
+    i = 1;
+    while (name[i])
+	{
+        if (!ft_isalnum(name[i]) && name[i] != '_') {
+            return 0;
+        }
+        i++;
+    }
+    return (1);
+}
+
+void update_env_var(const char *name, const char *value)
+{
+	if (setenv(name, value ? value : "", 1) == -1) {
+        perror("setenv");
+    }
+}
+
 void ft_handle_export(t_lexems *args, char **envp)
 {
+	t_lexems *current;
+	char **values_to_set_env;
+
  	if (args == NULL || args->next == NULL || ft_strchr(args->next->value, '=') == NULL)
 	{
         int i = 0;
@@ -58,6 +85,17 @@ void ft_handle_export(t_lexems *args, char **envp)
         }
         return;
     }
+	current = args->next;
+	while(current && ft_strchr(current->value, '='))
+	{
+		values_to_set_env = ft_split(current->value, '=');
+		if (values_to_set_env[0] && is_valid_env_name(values_to_set_env[0]))
+            update_env_var(values_to_set_env[0], values_to_set_env[1]);
+		else
+            ft_printf("export: `%s': not a valid identifier\n", current->value);
+		clean_args(values_to_set_env);
+		current = current->next;
+	}
 }
 
 int	execute_commands(t_exec_table *exec_table, char **envp)
