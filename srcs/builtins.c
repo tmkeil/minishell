e8d083ec92fd2e3eb85337d3b6ce7c525e5136c3
frocha <frocha@student.42heilbronn.de>
2024-12-19 19:57:25 +0100

    added functionality to unset existing variables from enevironemt variables

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -131,13 +131,43 @@ int	ft_changedir(t_lexems *lexems)
 
 int	ft_unset(t_lexems *lexems, t_env_node *envp_list)
 {
-	(void)lexems;
-	(void)envp_list;
+	t_env_node *previous;
+	t_env_node *current;
+	char *key_to_unset;
 
 	if (ft_strncmp(lexems->value, "unset", 6) == 0)
 	{
 		if (!lexems || !lexems->next)
+		{
 			ft_printf("unset: not enough arguments\n");
+			return (1);
+		}
+		
+		lexems = lexems->next;
+		while (lexems)
+		{
+			key_to_unset = lexems->value;
+
+			current = envp_list;
+			previous = NULL;
+			while (current)
+			{
+				if (ft_strncmp(current->name, key_to_unset, ft_strlen(key_to_unset) + 1) == 0)
+				{
+					if (previous)
+						previous->next = current->next;
+					else
+						envp_list = current->next;
+					free(current->name);
+					free(current->value);
+					free(current);
+					break;
+				}
+				previous = current;
+				current = current->next;
+			}
+			lexems = lexems->next;
+		}
 		return (1);
 	}
 	return (0);
